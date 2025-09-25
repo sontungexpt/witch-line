5ae5d9cdca829be15f32c7a1580e8c8af00296c5
Tung <sontungexpt@gmail.com>
2025-09-25 21:15:45 +0700

    enhances fvn1a 32 bit by auto casting to int32 by ffi (deduce bit.and call times)

--- lua/witch-line/utils/hash.lua
+++ lua/witch-line/utils/hash.lua
@@ -1,46 +1,44 @@
 local ffi = require("ffi")
-local bit = bit or require("bit")
 
 local M = {}
 
-local FNV_PRIME_32  = 0x01000193
-local FNV_OFFSET_32 = 0x811C9DC5
+local FNV_PRIME_32  = 0x01000193ULL
+local FNV_OFFSET_32 = 0x811C9DC5ULL
 
 ffi.cdef[[
+typedef uint32_t fnv32_t;
 typedef unsigned char uint8_t;
 ]]
 
---- Calculates the FNV-1a hash of a string using 32-bit FNV parameters.
---- Optimized with FFI pointer access.
+--- Calculates the FNV-1a 32-bit hash of a string.
+--- Uses FFI uint32_t for automatic 32-bit overflow (no bit.band needed).
 M.fnv1a32 = function(str)
     local len = #str
     local ptr = ffi.cast("const uint8_t*", str)
-    local hash = FNV_OFFSET_32
+    local hash = ffi.new("fnv32_t", FNV_OFFSET_32)
 
     for i = 0, len - 1 do
-        hash = bit.bxor(hash, ptr[i])
-        hash = bit.band(hash * FNV_PRIME_32, 0xFFFFFFFF)
+        hash = bit.bxor(hash, ptr[i]) * FNV_PRIME_32
     end
-    return hash
+    return tonumber(hash)
 end
 
---- Calculates the FNV-1a hash of concatenated strings.
+--- Calculates the FNV-1a 32-bit hash of concatenated strings.
 --- Accepts an array of strings and optional i..j range.
 M.fnv1a32_concat = function(strs, i, j)
-    local hash = FNV_OFFSET_32
     local first = i or 1
     local last  = j or #strs
+    local hash = ffi.new("fnv32_t", FNV_OFFSET_32)
 
     for l = first, last do
         local str = strs[l]
         local len = #str
         local ptr = ffi.cast("const uint8_t*", str)
         for k = 0, len - 1 do
-            hash = bit.bxor(hash, ptr[k])
-            hash = bit.band(hash * FNV_PRIME_32, 0xFFFFFFFF)
+            hash = bit.bxor(hash, ptr[k]) * FNV_PRIME_32
         end
     end
-    return hash
+    return tonumber(hash)
 end
 
 return M
