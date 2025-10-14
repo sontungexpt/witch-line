1e5406ae9a299564d11b2106a2696d54f2a79f56
Tran Vo Son Tung <sontungexpt@gmail.com>
2025-10-14 14:45:34 +0700

    add more inheritable fields

--- lua/witch-line/core/Component/inheritable_fields.lua
+++ lua/witch-line/core/Component/inheritable_fields.lua
@@ -3,12 +3,16 @@ return {
     lazy = true,
     events = true,
     user_events = true,
-    ref = true, -- Allow ref to easily access ref component directly instead of going through by parent
-    style = true,
     static = true,
     temp = true,
     context = true,
     min_screen_width = true,
     on_click = true,
+    ref = true, -- Allow ref to easily access ref component directly instead of going through by parent
+    style = true,
+    left = true,
+    right = true,
+    left_style = true,
+    right_style = true,
 }
 
